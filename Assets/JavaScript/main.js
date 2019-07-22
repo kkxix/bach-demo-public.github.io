1e82c5fe013e23990951d180567130dcea84a932
kknox1 <kknox1@swarthmore.edu>
2019-07-22 16:18:01 -0400

    samll fix

--- Assets/JavaScript/main.js
+++ Assets/JavaScript/main.js
@@ -26,15 +26,15 @@ if (urlParams.has('tempo') && urlParams.has('path')) {
 //Toggle dropdown 
 function dropDown() {
     document.getElementById("myDropdown").classList.toggle("show");
-    if(dropped) {
-        dropped = false
+    if (choraleDropped) {
+        choraleDropped = false
     } else {
-        dropped = true;
+        choraleDropped = true;
     }
 }
 document.addEventListener("click", function() {
     if(choraleDropped) {
-        dropDown()
+        dropDown();
     }
 })
 
