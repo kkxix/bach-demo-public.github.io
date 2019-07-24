456a975ff6316c48fd1ac0af4a3ef05e0ec4cadd
kknox1 <kknox1@swarthmore.edu>
2019-07-24 13:57:58 -0400

    modify alert

--- Assets/JavaScript/main.js
+++ Assets/JavaScript/main.js
@@ -12,7 +12,10 @@ var practice = false;
 var sentWhen = 0; 
 var inPath = ""; 
 var choraleDropped = false; 
-var practiceAlert = "Sorry but practice mode is not available for chorales for more than four voices at this time.";
+var practiceAlert = `
+                    Sorry but practice mode is not available for chorales 
+                    for more than four voices at this time. Please select a non-highlighted, 
+                    fourpart chorale in order to use practice mode.`.trim();
 var baseURL = "https://kkxix.github.io/bach-demo-public.github.io/"
 
 
